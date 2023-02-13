5f76e511230d70cce05118eef71ea60d61577390
Edixon Piña <edixonalbertto@gmail.com>
2023-02-13 19:17:39 -0400

    change url to import library cheerio

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export * from 'npm:husky'
 export * from 'https://deno.land/x/oak@v11.1.0/mod.ts'
-export * from 'https://esm.sh/cheerio@1.0.0-rc.12'
+export * from 'https://cdn.jsdelivr.net/npm/cheerio@1.0.0-rc.12/+esm'
 export { load as loadEnv } from 'https://deno.land/std@0.171.0/dotenv/mod.ts'
 export * from 'https://deno.land/x/superoak@4.7.0/mod.ts'
 export * from 'https://cdn.skypack.dev/preact-render-to-string'
