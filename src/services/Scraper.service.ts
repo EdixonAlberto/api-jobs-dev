fa8c5f2e3f433494c4a4cb57e3772b0fe3e3d7df
Edixon Piña <edixonalbertto@gmail.com>
2023-03-07 11:55:28 -0400

    correction in service scraper and library cheerio

--- src/services/Scraper.service.ts
+++ src/services/Scraper.service.ts
@@ -1,10 +1,11 @@
-import { CheerioAPI, load } from '$deps'
+import { CheerioAPI } from 'cheerio/types'
+import { load } from '$deps'
 
 export class ScraperService {
 	public async execute(url: string): Promise<CheerioAPI> {
 		const response = await fetch(url)
 		const html: string = await response.text()
-		const $ = load(html)
-		return $
+		const $ = load(html, null)
+		return $ as CheerioAPI
 	}
 }
